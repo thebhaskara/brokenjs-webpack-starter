2d1617e98b29dab479aeac0f851933ad247e47e5
bbusam_HA <bbusam@hostanalytics.com>
2018-03-27 11:34:50 +0530

    added utility creation

--- gulpfile.js
+++ gulpfile.js
@@ -24,34 +24,66 @@ var --upper-name = function() {
 export default Broken.ViewModel.make(--upper-name);
 `;
 
-var replaceAll = function(str, search, replace) {
+var jsUtilityFileContent = `
+import Broken from "broken";
+import _ from "lodash";
+
+var --upper-name = function() {
+    
+}
+
+--upper-name.prototype = {
+
+}
+
+export default --upper-name;
+`;
+
+var replaceAll = function (str, search, replace) {
     return str.replace(new RegExp(search, 'g'), replace);
 }
 
-gulp.task('add', function() {
+gulp.task('add', function () {
     var dest;
     if (argv.component) dest = 'components';
     else if (argv.page) dest = 'pages';
+    else if (argv.utility) dest = 'utilities';
     else throw "specify --component or --page";
 
-    var name = argv.component || argv.page;
+    var name = argv.component || argv.page || argv.utility;
     name = _.kebabCase(name);
     if (!_.isString(name)) throw "specify name";
 
-    var dir = ['modules', dest, name].join('/');
-
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
+    if (!fs.existsSync('modules/' + dest)) {
+        fs.mkdirSync('modules/' + dest);
     }
 
-    var path = [dir, name].join('/')
+    if (argv.utility) {
 
-    fs.writeFileSync(path + '.less', '.view-name {}');
-    fs.writeFileSync(path + '.html', '<div></div>');
+        var content = replaceAll(jsUtilityFileContent, '--name', name);
+        content = replaceAll(content, '--upper-name', _.upperFirst(_.camelCase(name)))
 
-    var content = replaceAll(jsFileContent, '--name', name);
-    content = replaceAll(content, '--upper-name', _.upperFirst(_.camelCase(name)))
-    fs.writeFileSync(path + '.js', content);
+        var path = ['modules', dest, name].join('/') + '.js';
+        fs.writeFileSync(path, content);
 
-    console.log('made ' + dir + ', enjoy!');
+        console.log('made ' + dest + " " + path + ', enjoy!');
+    } else {
+
+        var dir = ['modules', dest, name].join('/');
+
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+
+        var path = [dir, name].join('/')
+
+        fs.writeFileSync(path + '.less', '.view-name {}');
+        fs.writeFileSync(path + '.html', '<div></div>');
+
+        var content = replaceAll(jsFileContent, '--name', name);
+        content = replaceAll(content, '--upper-name', _.upperFirst(_.camelCase(name)))
+        fs.writeFileSync(path + '.js', content);
+        console.log('made ' + dest + " " + dir + ', enjoy!');
+    }
+
 })
